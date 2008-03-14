f522621df87e63f6bba41dddee0ec92eb5438dd4
tarragon <tarragon>
2008-03-14 12:50:43 +0000

    Changed shebang path. Changed GHROOT. (GHROOT should be a command line option, environment var, or determine itself automatically) Added gh3-x360 to list of games. Changed processing order to song/diff rather than diff/song. Skipped to latest DLC.

--- bin/oldbin/doit.pl
+++ bin/oldbin/doit.pl
@@ -1,15 +1,15 @@
-#!/usr/bin/perl5
+#!/usr/bin/perl -w
+use strict;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use SongLib;
-use strict;
 
-our $GHROOT = "/home/Dave/gh";
+our $GHROOT = "/home/tarragon/cvs/spopt";
 our $SL = new SongLib;
-foreach my $game (qw(gh2-ps2 gh2-x360 gh-ps2 ghrt80s-ps2 gh3-ps2)) {
+foreach my $game (qw(gh2-ps2 gh2-x360 gh-ps2 ghrt80s-ps2 gh3-ps2 gh3-x360)) {
     my @songarr = $SL->get_songarr_for_game($game);
-    foreach my $diff (qw(expert hard medium easy)) {
-	foreach my $song (@songarr) {
+    foreach my $song (@songarr) {
+        foreach my $diff (qw(expert hard medium easy)) {
 	    my $tier  = $song->{tier};
 	    my $title = $song->{name};
 	    my $file  = $song->{file};
@@ -18,8 +18,8 @@ foreach my $game (qw(gh2-ps2 gh2-x360 gh-ps2 ghrt80s-ps2 gh3-ps2)) {
 	    ##next unless $tier == 10;
 	    ##next unless $file =~ /carcinogen|tina|puttingholes|morello|slash/;
 	    ##next unless $diff eq "expert";
-	    next unless $game eq "gh2-x360"; 
-	    next unless $file =~ /memoriesofthe|stateofmass|youshouldbeashamed/;
+	    next unless $game eq "gh3-x360"; 
+	    unless ( $file =~ /^dlc38/ or $file =~ /^dlc39/ or $file =~ /^dlc40/ ) { next };
 	    system("perl $GHROOT/bin/process_song.pl $game $diff $tier \"$title\" $file");
 	    ##system("perl $GHROOT/bin/process_song.pl $game $diff $tier \"$title\" $file $sust");
 	}
